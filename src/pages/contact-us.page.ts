0b267fbd8bd88e17f78e04bf229011191fdc3e3f
Jonas Claes <jonas.claes@qaura.be>
2024-09-04 18:19:40 +0200

    fix: update contact us link

--- src/pages/contact-us.page.ts
+++ src/pages/contact-us.page.ts
@@ -2,7 +2,7 @@ import { BasePage } from "@framework/base.page.js";
 import { Locator } from "@playwright/test";
 
 export class ContactUsPage extends BasePage {
-  readonly url = "/contactus";
+  readonly url = "https://www.qaura.be/contactus";
 
   readonly headingContactUs: Locator = this.page
     .locator("#wrap")
