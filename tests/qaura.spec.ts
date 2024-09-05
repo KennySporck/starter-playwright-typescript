2be61a2029008bd05b043ff481ef8c8fdc11a236
Jonas Claes <jonas.claes@qaura.be>
2024-09-05 09:39:23 +0200

    refactor: QAura into a suite

--- tests/qaura.spec.ts
+++ tests/qaura.spec.ts
@@ -1,40 +1,42 @@
 import { test } from "@framework/test.js";
 import { expect } from "@playwright/test";
 
-test("has logo", async ({ page, homePage }) => {
-  await page.goto(homePage.url);
+test.describe("QAura", () => {
+  test("has logo", async ({ page, homePage }) => {
+    await page.goto(homePage.url);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Home/);
-  await expect(page).toHaveTitle(/QAura/);
-});
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Home/);
+    await expect(page).toHaveTitle(/QAura/);
+  });
 
-test("has introduction", async ({ page, homePage }) => {
-  await page.goto(homePage.url);
+  test("has introduction", async ({ page, homePage }) => {
+    await page.goto(homePage.url);
 
-  // Expect the introduction to have the exact text.
-  await expect(homePage.headingIntroduction).toHaveText(
-    "At QAura, we are dedicated to redefining quality assurance with a focus on innovation and sustainability. Our advanced software testing solutions empower businesses to create robust and eco-friendly digital products.",
-  );
+    // Expect the introduction to have the exact text.
+    await expect(homePage.headingIntroduction).toHaveText(
+      "At QAura, we are dedicated to redefining quality assurance with a focus on innovation and sustainability. Our advanced software testing solutions empower businesses to create robust and eco-friendly digital products.",
+    );
 
-  // Expect the page to be fully loaded before taking a screenshot.
-  await page.waitForLoadState("networkidle");
+    // Expect the page to be fully loaded before taking a screenshot.
+    await page.waitForLoadState("networkidle");
 
-  // Expect that it looks correct.
-  await expect(homePage.headingIntroduction).toHaveScreenshot();
-});
+    // Expect that it looks correct.
+    await expect(homePage.headingIntroduction).toHaveScreenshot();
+  });
 
-test("has contact us link", async ({ page, homePage, contactUsPage }) => {
-  await page.goto(homePage.url);
+  test("has contact us link", async ({ page, homePage, contactUsPage }) => {
+    await page.goto(homePage.url);
 
-  // Click the contact us link.
-  await homePage.linkContactUs.click();
+    // Click the contact us link.
+    await homePage.linkContactUs.click();
 
-  // Expects page to have a heading with the name of Contact Us.
-  await expect(page).toHaveTitle(/Contact Us/);
+    // Expects page to have a heading with the name of Contact Us.
+    await expect(page).toHaveTitle(/Contact Us/);
 
-  await expect(contactUsPage.headingContactUs).toBeVisible();
+    await expect(contactUsPage.headingContactUs).toBeVisible();
 
-  // Expects page to have a URL of /contactus.
-  await expect(page).toHaveURL(contactUsPage.url);
+    // Expects page to have a URL of /contactus.
+    await expect(page).toHaveURL(contactUsPage.url);
+  });
 });
