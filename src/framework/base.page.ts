998d9fcccf571bbbc9151af128f0bc3299fd0ba6
Jonas Claes <jonas.claes@qaura.be>
2024-09-05 10:16:45 +0200

    refactor: expose page from pom

--- src/framework/base.page.ts
+++ src/framework/base.page.ts
@@ -1,5 +1,5 @@
 import { Page } from "@playwright/test";
 
 export class BasePage {
-  constructor(protected readonly page: Page) {}
+  constructor(public readonly page: Page) {}
 }
--- tests/qaura.spec.ts
+++ tests/qaura.spec.ts
@@ -5,38 +5,27 @@ test.describe("QAura", () => {
   test("has logo", async ({ page, homePage }) => {
     await page.goto(homePage.url);
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Home/);
-    await expect(page).toHaveTitle(/QAura/);
+    await expect(homePage.page).toHaveTitle(/Home/);
+    await expect(homePage.page).toHaveTitle(/QAura/);
   });
 
   test("has introduction", async ({ page, homePage }) => {
     await page.goto(homePage.url);
 
-    // Expect the introduction to have the exact text.
     await expect(homePage.headingIntroduction).toHaveText(
       "At QAura, we are dedicated to redefining quality assurance with a focus on innovation and sustainability. Our advanced software testing solutions empower businesses to create robust and eco-friendly digital products.",
     );
-
-    // Expect the page to be fully loaded before taking a screenshot.
-    await page.waitForLoadState("networkidle");
-
-    // Expect that it looks correct.
+    await homePage.page.waitForLoadState("networkidle");
     await expect(homePage.headingIntroduction).toHaveScreenshot();
   });
 
   test("has contact us link", async ({ page, homePage, contactUsPage }) => {
     await page.goto(homePage.url);
 
-    // Click the contact us link.
     await homePage.linkContactUs.click();
 
-    // Expects page to have a heading with the name of Contact Us.
-    await expect(page).toHaveTitle(/Contact Us/);
-
+    await expect(contactUsPage.page).toHaveTitle(/Contact Us/);
     await expect(contactUsPage.headingContactUs).toBeVisible();
-
-    // Expects page to have a URL of /contactus.
-    await expect(page).toHaveURL(contactUsPage.url);
+    await expect(contactUsPage.page).toHaveURL(contactUsPage.url);
   });
 });
