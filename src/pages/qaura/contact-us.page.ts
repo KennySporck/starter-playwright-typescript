e9f0e9a5c988ea491583667d5000aed7f494005e
Jonas Claes <jonas.claes@qaura.be>
2024-09-05 11:15:55 +0200

    refactor: rename url to href

--- src/pages/qaura/contact-us.page.ts
+++ src/pages/qaura/contact-us.page.ts
@@ -2,8 +2,7 @@ import { BasePage } from "@framework/base.page.js";
 import { Locator } from "@playwright/test";
 
 export class ContactUsPage extends BasePage {
-  readonly url = "https://www.qaura.be/contactus";
-
+  readonly href = "https://www.qaura.be/contactus";
   readonly headingContactUs: Locator = this.page
     .locator("#wrap")
     .getByRole("list")
--- tests/qaura.spec.ts
+++ tests/qaura.spec.ts
@@ -11,11 +11,11 @@ test.describe("QAura", () => {
 
   test("has introduction", async ({ homePage }) => {
     await homePage.page.goto(homePage.href);
+    await homePage.page.waitForLoadState("networkidle");
 
     await expect(homePage.headingIntroduction).toHaveText(
       "At QAura, we are dedicated to redefining quality assurance with a focus on innovation and sustainability. Our advanced software testing solutions empower businesses to create robust and eco-friendly digital products.",
     );
-    await homePage.page.waitForLoadState("networkidle");
     await expect(homePage.headingIntroduction).toHaveScreenshot();
   });
 
@@ -26,6 +26,6 @@ test.describe("QAura", () => {
 
     await expect(contactUsPage.page).toHaveTitle(/Contact Us/);
     await expect(contactUsPage.headingContactUs).toBeVisible();
-    await expect(contactUsPage.page).toHaveURL(contactUsPage.url);
+    await expect(contactUsPage.page).toHaveURL(contactUsPage.href);
   });
 });
